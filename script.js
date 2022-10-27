4a35edc28f4655e4cc6590c7c71dba0968be4f50
nadege.derville <nadege.derville@gmail.com>
2022-10-27 10:58:09 +0200

    project done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log();
+
+const showMore = document.querySelector(".show-more");
+const descriptionDetail = document.querySelector(".description-detail");
+console.log(showMore);
+showMore.addEventListener('click', function() {
+    descriptionDetail.classList.toggle("show")
+});
+
+//const showMore = document.querySelector(".show-more"); variable déjà déclarée au-dessus
+const genesisDetail = document.querySelector(".genesis-detail");
+console.log(showMore);
+showMore.addEventListener('click', function() {
+    genesisDetail.classList.toggle("show")
+});
